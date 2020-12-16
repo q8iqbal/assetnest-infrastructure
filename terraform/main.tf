45751404ecc9e8aa4fec46f75367da7c035afd51
iqbal nur imansyah <iqbalnurimansyah@gmail.com>
2020-12-16 22:00:22 +0700

    dah bisa jalan droplet nya + bisa dipakek request

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,18 +37,47 @@ resource "digitalocean_droplet" "assetnest" {
         data.digitalocean_ssh_key.windows.id,
         data.digitalocean_ssh_key.linux.id,
     ]
+}
+
+resource "null_resource" "temp" {
+    count = var.droplet_count
 
     provisioner "file"{
         source = "../docker/assetnest"
         destination = "/home"
         connection {
-            host     = digitalocean_droplet.assetnest[count.index].ipv4_address
+            type = "ssh"
+            user = "root"
+            host = digitalocean_droplet.assetnest[count.index].ipv4_address
         }
     }
 
     provisioner "local-exec" {
         command = "echo '${digitalocean_droplet.assetnest[count.index].name} ansible_host=${digitalocean_droplet.assetnest[count.index].ipv4_address} ansible_ssh_user=root ansible_python_interpreter=/usr/bin/python3' >> ../ansible/hosts"
     }
+}
+
+//nambah dns record
+
+resource "digitalocean_domain" "default" {
+    name = "assetnest.me"
+}
+
+# Add an A record to the domain for www.example.com.
+resource "digitalocean_record" "entah" {
+    count = var.droplet_count
+    domain = digitalocean_domain.default.name
+    type   = "A"
+    name   = "@"
+    value  = digitalocean_droplet.assetnest[count.index].ipv4_address
+}
+
+resource "digitalocean_record" "api" {
+    count = var.droplet_count
+    domain = digitalocean_domain.default.name
+    type   = "A"
+    name   = "api"
+    value  = digitalocean_droplet.assetnest[count.index].ipv4_address
 }
 
 output "server_ip" {
